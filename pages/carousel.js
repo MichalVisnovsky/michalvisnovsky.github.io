cf7a128c2eb78988e1a8689fb47e831aae099dc4
visnovskymichal0@gmail.com <visnovskymichal0@gmail.com>
2024-04-14 19:04:46 +0200

    fix

--- pages/carousel.js
+++ pages/carousel.js
@@ -162,19 +162,19 @@ new Carousel({
 	target: document.querySelector('.carousel'),
   items: [
   	{
-    	image: "/assets/coffee_trip/carousel-1.png"
+    	image: "../assets/coffee_trip/carousel-1.png"
     },
   	{
-    	image: "/assets/coffee_trip/carousel-2.png"
+    	image: "../assets/coffee_trip/carousel-2.png"
     },
   	{
-    	image: "/assets/coffee_trip/carousel-3.png"
+    	image: "../assets/coffee_trip/carousel-3.png"
     },
   	{
-    	image: "/assets/coffee_trip/carousel-4.png"
+    	image: "../assets/coffee_trip/carousel-4.png"
     },
   	{
-    	image: "/assets/coffee_trip/carousel-5.png"
+    	image: "../assets/coffee_trip/carousel-5.png"
     }
   ]
 });
