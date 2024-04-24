c503bbc82996f47b6f30b5c201fefd2dabd13fdc
visnovskymichal0@gmail.com <visnovskymichal0@gmail.com>
2024-04-24 22:27:12 +0200

    finished (hopefully)

--- pages/carousel.js
+++ pages/carousel.js
@@ -157,23 +157,4 @@ class Carousel {
   }
 }
 
-new Carousel({
-	target: document.querySelector('.carousel'),
-  items: [
-  	{
-    	image: "../assets/coffee_trip/carousel-1.png"
-    },
-  	{
-    	image: "../assets/coffee_trip/carousel-2.png"
-    },
-  	{
-    	image: "../assets/coffee_trip/carousel-3.png"
-    },
-  	{
-    	image: "../assets/coffee_trip/carousel-4.png"
-    },
-  	{
-    	image: "../assets/coffee_trip/carousel-5.png"
-    }
-  ]
-});
+export default Carousel;
